a7d48245a80c0cd3273d3cd7edf04bedbe991e62
nahuboca7 <nahuboca7@gmail.com>
2024-10-08 23:04:09 -0300

    Ejercicio7Poo

---
+++ EjerciciosPoo/Ejercicio7Poo.py
@@ -0,0 +1,12 @@
+"""7. Expocoches. Escribe un programa de consola para gestionar la venta de entradas (no
+ numeradas) de Expocoches que tiene 3 zonas, la sala principal con 1000 entradas
+ disponibles, la zona de compra-venta con 200 entradas disponibles y la zona vip con
+ 25 entradas disponibles. Hay que controlar que existen entradas antes de venderlas.
+ Define las clase Zona con sus atributos y métodos correspondientes y crea un
+ programa de consola que permita vender las entradas.
+ El menú del programa debe ser el siguiente: 1- Mostrar número de entradas libres,
+ 2- Vender entradas y 3- Salir
+ Cuando elegimos la opción 2, se nos debe preguntar para qué zona queremos las
+ entradas y cuántas queremos. Lógicamente, el programa debe controlar que no se
+ puedan vender más entradas de la cuenta."""
+
